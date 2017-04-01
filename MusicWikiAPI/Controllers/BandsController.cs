a2bb8fa0ff1892aea4ede3d4d0a462a76dd6ef40
Domen Perenic <domc987@gmail.com>
2017-04-01 19:41:06 +0200

    Project init. Library for dbEntites. Api with models.

---
+++ MusicWikiAPI/Controllers/BandsController.cs
@@ -0,0 +1,30 @@
+﻿using CRUDLibrary;
+using MusicWikiAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicWikiAPI.Controllers
+{
+    public class BandsController : ApiController
+    {
+        musicwikiEntities entities = new musicwikiEntities();
+
+        public IQueryable<BandDTO> Get()
+        {
+            var bands = from b in entities.bands
+                        select new BandDTO() {
+                            id = b.id,
+                            name = b.name,
+                            country = b.country,
+                            genre = b.genre,
+                            formationDate = b.formationDate
+                        };
+
+            return bands;
+        }
+    }
+}
---
+++ MusicWikiAPI/Models/BandModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicWikiAPI.Models
+{
+    public class BandDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public System.DateTime formationDate { get; set; }
+        public string genre { get; set; }
+    }
+}
