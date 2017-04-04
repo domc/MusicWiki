08c53a3c642685b03fe173e1634b19ab81eeb537
Domen Perenic <domc987@gmail.com>
2017-04-04 16:19:53 +0200

    Added filtering/soritng of musicians.

--- MusicWikiAPI/app/members/membersCtrl.js
+++ MusicWikiAPI/app/members/membersCtrl.js
@@ -1,6 +1,9 @@
 ﻿angular.module("MusicWikiApp")
    .controller("membersCtrl", ['$scope', 'membersService', function ($scope, membersService) {
        $scope.pageTitle = 'List of members';
+       $scope.sortBy = "lastName"; //default sorting by last name
+       $scope.sortDescending = false; // default ascending
+       $scope.searchText = ''; // default blank
        getAllMembers();
 
        function getAllMembers() {
