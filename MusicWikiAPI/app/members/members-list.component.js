6ce4132b3dc8c37d431845c976cfd75be56ad7b2
Domen Perenic <domc987@gmail.com>
2017-04-04 18:35:29 +0200

    List of musicians -> component.

---
+++ MusicWikiAPI/app/members/members-list.component.js
@@ -0,0 +1,19 @@
+﻿MusicWikiApp.component('membersList', {
+    templateUrl: 'app/members/members-list.template.html',
+    controller: function MembersListController(membersService) {
+        this.sortBy = "firstName"; //default sorting by last name
+        this.sortDescending = false; // default ascending
+        this.searchText = ''; // default blank
+
+        getAllMembers(this); // retrieve all bands from db
+
+        function getAllMembers(controller) {
+            var servCall = membersService.getMembers();
+            servCall.then(function (d) {
+                controller.members = d.data;
+            }, function (error) {
+                controller.error('Oops! Something went wrong while fetching the data.');
+            });
+        }
+    }
+});
--- MusicWikiAPI/app/members/membersCtrl.js
+++ MusicWikiAPI/app/members/membersCtrl.js
@@ -1,16 +1,3 @@
-﻿MusicWikiApp.controller("membersCtrl", ['$scope', 'membersService', function ($scope, membersService) {
-    $scope.pageTitle = 'List of members';
-    $scope.sortBy = "lastName"; //default sorting by last name
-    $scope.sortDescending = false; // default ascending
-    $scope.searchText = ''; // default blank
-    getAllMembers();
-
-    function getAllMembers() {
-        var servCall = membersService.getMembers();
-        servCall.then(function (d) {
-            $scope.members = d.data;
-        }, function (error) {
-            $scope.error('Oops! Something went wrong while fetching the data.')
-        })
-    }
+﻿MusicWikiApp.controller("membersCtrl", ['$scope', function ($scope) {
+    $scope.pageTitle = 'List of all musicians';
 }]);
