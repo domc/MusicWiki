88cd1983478e15375aa574e3669879fad27d7f48
Domen Perenic <domc987@gmail.com>
2017-04-04 14:50:03 +0200

    Added angular (with UI Route) and first sample pages.

---
+++ MusicWikiAPI/app/app.js
@@ -0,0 +1,23 @@
+﻿angular.module('MyApp', ['ui.router'])
+  .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+
+      // default route
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+          .state('bands', {
+              url: '/',
+              templateUrl: 'app/bands/bands.html',
+              controller: 'bandsCtrl'
+          })
+          .state('members', {
+              url: '/members',
+              templateUrl: 'app/members/members.html',
+              controller: 'membersCtrl'
+          })
+          .state('albums', {
+              url: '/albums',
+              templateUrl: 'app/albums/albums.html',
+              controller: 'albumsCtrl'
+          })
+  }]);
---
+++ MusicWikiAPI/app/bands/bandsCtrl.js
@@ -0,0 +1,5 @@
+﻿angular.module("MyApp")
+   .controller("bandsCtrl", ['$scope', function ($scope) {
+       $scope.pageTitle = 'home page';
+       $scope.message = 'This is the message from controller to view on page';
+   }]);
