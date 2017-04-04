fce4bc9625efcf196a12f661ebbfbf90085c62ed
Domen Perenic <domc987@gmail.com>
2017-04-04 17:58:58 +0200

    List of bands -> component.

--- MusicWikiAPI/app/bands/bandsCtrl.js
+++ MusicWikiAPI/app/bands/bandsCtrl.js
@@ -1,18 +1,3 @@
 ﻿MusicWikiApp.controller("bandsCtrl", ['$scope', 'bandsService', function ($scope, bandsService) {
     $scope.pageTitle = 'List of all bands';
-    $scope.sortBy = "name"; //default sorting by name
-    $scope.sortDescending = false; // default ascending
-    $scope.searchText = ''; // default blank
-
-       
-    getAllBends(); // retrieve all bands from db
-
-    function getAllBends() {
-        var servCall = bandsService.getBands();
-        servCall.then(function (d) {
-            $scope.bands = d.data;
-        }, function (error) {
-            $scope.error('Oops! Something went wrong while fetching the data.')
-        })
-    }
 }]);
