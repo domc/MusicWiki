2aa81f8e06de947922ca1be53d89e026547f88f4
Domen Perenic <domc987@gmail.com>
2017-04-06 16:55:15 +0200

    Enabled deletion of bands from db.

--- MusicWikiAPI/app/bands/band-detail/band-detail.component.js
+++ MusicWikiAPI/app/bands/band-detail/band-detail.component.js
@@ -2,7 +2,20 @@
   module('bandDetail').
   component('bandDetail', {
       templateUrl: 'app/bands/band-detail/band-detail.template.html',
-      controller: ['$stateParams', 'Band', function BandDetailController($stateParams, Band) {
+      controller: ['$stateParams', '$location', 'Band', function BandDetailController($stateParams, $location, Band) {
           this.band = Band.get({ bandId: $stateParams.id });
+          this.deleteBand = function () {
+              var ans = confirm('Are you sure you want to remove this band?');
+              if (ans)
+              {
+                  Band.delete({ bandId: $stateParams.id },
+                      function (band, ResponseHeaders, statusCode, statusText) {
+                        alert(statusCode+" "+statusText);
+                        $location.url('/bands');
+                  }, function (httpResponse) {
+                      alert(httpResponse.status+" "+httpResponse.statusText);
+                  });
+              }              
+          }
       }]
   });
