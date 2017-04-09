52da0f61a9c12175b523f21202121797f51ed43f
Domen Perenic <domc987@gmail.com>
2017-04-09 15:32:04 +0200

    Updated createForm.

--- MusicWikiAPI/app/bands/band-create/band-create.component.js
+++ MusicWikiAPI/app/bands/band-create/band-create.component.js
@@ -3,19 +3,21 @@
   component('bandCreate', {
       templateUrl: 'app/bands/band-create/band-create.template.html',
       controller: ['Band', '$location', function BandCreateController(Band, $location) {
-          this.saveBand = function () {
-              var band = {
-                  'name': this.bandName,
-                  'country': this.country,
-                  'formationDate': this.formationDate,
-                  'genre': this.genre
-              };
-              Band.save(band, function (band, ResponseHeaders, statusCode, statusText) {
-                  alert(statusCode+" "+statusText);
-                  $location.url('/bands');
-              }, function (httpResponse) {
-                  alert(httpResponse.status+" "+httpResponse.statusText);
-              });
+          this.saveBand = function (formValidationCheck) {
+              if (formValidationCheck) {
+                  var band = {
+                      'name': this.bandName,
+                      'country': this.country,
+                      'formationDate': this.formationDate,
+                      'genre': this.genre
+                  };
+                  Band.save(band, function (band, ResponseHeaders, statusCode, statusText) {
+                      alert(statusCode + " " + statusText);
+                      $location.url('/bands');
+                  }, function (httpResponse) {
+                      alert(httpResponse.status + " " + httpResponse.statusText);
+                  });
+              }
           }
       }]
   });
